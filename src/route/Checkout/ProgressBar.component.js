55979d6c228f79dae4ffb520f47d28a0d77b59c6
greatT1me <99392118+greatT1me@users.noreply.github.com>
2022-10-28 20:35:55 +0400

    Add results

---
+++ src/route/Checkout/ProgressBar.component.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import './ProgressBar.component.css'
+
+export class ProgressBar extends Component {
+  render() {
+    const { checkoutStep , stepMap} = this.props;
+    const stepKeys= Object.keys(stepMap);
+    const stepAmmount=stepKeys.length;
+    // const stepWidth=100/stepAmmount;
+    const activeIndex=stepKeys.indexOf(checkoutStep)
+    const stepWidth = (100 / stepAmmount) + "%";
+    const width=`calc(${stepWidth}%-15px)`
+    console.log(checkoutStep)
+    return (
+      <div className='progress_bar'>
+        {/* <div className='progress_bar_line' style={{width:`calc(${stepWidth}%-15px)`}}></div> */}
+        {stepKeys.map((key, i)=>{
+          const active= i<=activeIndex;
+          console.log(active)
+            return(
+                <div className='progress_bar_grid '>
+                    <div className= {`progress_bar_line ${active? 'active_step': null}`} ></div>
+                    { i==stepAmmount-1?
+                      <div className= {`progress_bar_line `} >
+                        {/* {active?<div className='active_step_line' ></div>: null} */}
+                        <div className={` ${active? 'active_step_line': null}`}></div>
+                      </div> :
+                      <div className={`step_circle flex ${active? 'active_step': null}`} >{i+1}</div>
+                    }
+                    
+                </div>
+                
+            )
+        })}
+      </div>
+    )
+  }
+}
+
+export default ProgressBar;
